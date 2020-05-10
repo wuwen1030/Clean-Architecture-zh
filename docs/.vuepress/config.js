be44b7afbf80996d604f0598a3510a3ac6e45482
gdut-yy <gdut_yy@163.com>
2020-05-10 16:18:19 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,31 +1,17 @@
 // .vuepress/config.js
 module.exports = {
-  // 网站的标题
-  title: "Clean Architecture 中文",
-  // 上下文根
+  title: "《架构整洁之道》中文翻译",
   base: "/doc-cleanarch/",
   themeConfig: {
-    // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
     repo: "gdut-yy/Clean-Architecture-zh",
-    // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
-    // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
     repoLabel: "Github",
-    // 以下为可选的编辑链接选项
-    // 假如你的文档仓库和项目本身不在一个仓库：
     docsRepo: "gdut-yy/Clean-Architecture-zh",
-    // 假如文档放在一个特定的分支下：
     docsBranch: "master/docs",
-    // 默认是 false, 设置为 true 来启用
     editLinks: true,
-    // 默认为 "Edit this page"
     editLinkText: "帮助我们改善此页面！",
-    // 最后更新时间
     lastUpdated: "Last Updated",
-    // 最大深度
     sidebarDepth: 2,
-    // 导航栏
     nav: [],
-    // 侧边栏
     sidebar: {
       "/": [
         "",
@@ -69,8 +55,8 @@ module.exports = {
         "ch32.md",
         "ch33.md",
         "ch34.md",
-        "afterword.md"
-      ]
-    }
-  }
+        "afterword.md",
+      ],
+    },
+  },
 };
